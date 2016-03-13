bc43ac6b0cc44f612dacdb434e40365845f7daa5
Elek <elzbieta.labaj@gmail.com>
2016-03-13 18:41:23 +0100

    The best first test

---
+++ GameOfLife/GameOfLife.Tests/MyTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GameOfLife.Tests
+{
+    [TestFixture]
+    class MyTest
+    {
+        [Test]
+        public void Test4()
+        {
+            Cell currentCell = new Cell() { IsAlive = true };
+
+            bool result = F.WhatsNext(currentCell, 1);
+
+            Assert.IsFalse(result);
+        }
+
+    }
+}
---
+++ GameOfLife/GameOfLife/Cell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class Cell
+    {
+        public bool IsAlive { get; set; }
+    }
+    public class anotherClass
+    {
+        public static bool WhatsNext(Cell currentCell, int aliveNeighbours)
+        {
+            if (currentCell.IsAlive && aliveNeighbours <= 2)
+                return false;
+            return true;
+        }
+    }
+}
