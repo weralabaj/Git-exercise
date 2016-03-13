f449eefc33e2850d7c9cebffe22831162929e5e7
emel <d3only@interia.pl>
2016-03-13 18:41:11 +0100

    firsttest

---
+++ GameOfLife/GameOfLife.Tests/GameOfLifeTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GameOfLife.Tests
+{
+    [TestFixture]
+    public class GameOfLifeTests
+    {
+       
+            [Test]
+            public void GameOfLifeTestMethod()
+            {
+                Cell currentCell = new Cell() { IsAlive = true };
+
+                bool result = RulesOfLife.WhatsNext(currentCell, 1);
+
+                Assert.IsFalse(result);
+            }
+
+           
+    
+}
+}
